bcd47205d32a0eba3f54c6be9cc13c524196d2c4
Luis Eduardo Imbernón <imbernon@gmail.com>
2021-06-05 15:54:59 +0200

    Added the difference time in seconds between the last login and the action.

--- src/formatcsv.py
+++ src/formatcsv.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import sys
+from datetime import datetime
 
 # Function to load the json file data
 def loadjsondata(filepath):
@@ -27,125 +28,126 @@ def getdifferentskills(tmpdata):
 
 def writepedagogicalsoftwareinterventionscsv(interventions):
 
-    rowList=[]
-    rowList.append(['studentGender', 'studentMotherTongue', 'studentAge', 'studentCompetence',
-                    'studentMotivation', 'exerciseSkillParalellism', 'exerciseSkillLogicalThinking',
-                    'exerciseSkillFlowControl', 'exerciseSkillUserInteractivity', 'exerciseSkillInformationRepresentation',
-                    'exerciseSkillAbstraction', 'exerciseSkillSyncronization', 'exerciseValidSolution', 'exerciseIsEvaluation',
-                    'exerciseLevel', 'solutionDistanceFamilyDistance', 'solutionDistanceElementDistance',
-                    'solutionDistancePositionDistance', 'solutionDistanceInputDistance', 'solutionDistanceTotalDistance',
-                    'dateTime', 'requestHelp', 'secondsHelpOpen', 'finishedExercise', 'validSolution', 'grade',
-                    'lastLogin'])
+    row_list=[]
+    row_list.append(['student_gender', 'student_mother_tongue', 'student_age', 'student_competence',
+                    'student_motivation', 'exercise_skill_paralellism', 'exercise_skill_logical_thinking',
+                    'exercise_skill_flow_control', 'exercise_skill_user_interactivity', 'exercise_skill_information_representation',
+                    'exercise_skill_abstraction', 'exercise_skill_syncronization', 'exercise_valid_solution', 'exercise_is_evaluation',
+                    'exercise_level', 'solution_distance_family_distance', 'solution_distance_element_distance',
+                    'solution_distance_position_distance', 'solution_distance_input_distance', 'solution_distance_total_distance',
+                    'request_help', 'seconds_help_open', 'finished_exercise', 'valid_solution', 'grade', 'totalSeconds'])
 
     for element in interventions:
 
-        dateTime = None
-        lastLogin = None
-        studentGender = None
-        studentAge = None
-        studentMotherTongue = 0
-        studentCompetence = 0
-        studentMotivation = 0
+        student_gender = None
+        student_age = None
+        total_seconds = None
+        student_mother_tongue = 0
+        student_competence = 0
+        student_motivation = 0
 
-        exerciseSkillParalellism = 0
-        exerciseSkillLogicalThinking = 0
-        exerciseSkillFlowControl = 0
-        exerciseSkillUserInteractivity = 0
-        exerciseSkillInformationRepresentation = 0
-        exerciseSkillAbstraction = 0
-        exerciseSkillSyncronization = 0
-        exerciseValidSolution = 0
-        exerciseLevel = 0
-        solutionDistanceFamilyDistance = 0
-        solutionDistanceElementDistance = 0
-        solutionDistancePositionDistance = 0
-        solutionDistanceInputDistance = 0
-        solutionDistanceTotalDistance = 0
-        secondsHelpOpen = 0
-        validSolution = 0
+        exercise_skill_paralellism = 0
+        exercise_skill_logical_thinking = 0
+        exercise_skill_flow_control = 0
+        exercise_skill_user_interactivity = 0
+        exercise_skill_information_representation = 0
+        exercise_skill_abstraction = 0
+        exercise_skill_syncronization = 0
+        exercise_valid_solution = 0
+        exercise_level = 0
+        solution_distance_family_distance = 0
+        solution_distance_element_distance = 0
+        solution_distance_position_distance = 0
+        solution_distance_input_distance = 0
+        solution_distance_total_distance = 0
+        seconds_help_open = 0
+        valid_solution = 0
         grade = 0
 
-        exerciseIsEvaluation = False
-        requestHelp = False
-        finishedExercise = False
+        exercise_is_evaluation = False
+        request_help = False
+        finished_exercise = False
+
+        # Time calculation between the last login and the current action
+        if 'dateTime' in element and 'lastLogin' in element:
+            date_time_obj = datetime.strptime(element['dateTime'], '%Y-%m-%d %H:%M:%S.%f')
+            last_login_obj = datetime.strptime(element['lastLogin'], '%Y-%m-%d %H:%M:%S')
+            difference = (date_time_obj - last_login_obj)
+            total_seconds = difference.total_seconds()
+
 
         # Student information
         if 'student' in element:
             if 'gender' in element['student']:
-                studentGender = element['student']['gender']
+                student_gender = element['student']['gender']
             if 'age' in element['student']:
-                studentAge = element['student']['age']
+                student_age = element['student']['age']
             if 'motherTongue' in element['student']:
-                studentMotherTongue = element['student']['motherTongue']
+                student_mother_tongue = element['student']['motherTongue']
             if 'competence' in element['student']:
-                studentCompetence = element['student']['competence']
+                student_competence = element['student']['competence']
             if 'motivation' in element['student']:
-                studentMotivation = element['student']['motivation']
+                student_motivation = element['student']['motivation']
 
         # Exercise information
         if 'exercise' in element:
             if 'skills' in element['exercise']:
                 for skill in element['exercise']['skills']:
                     if skill['name'] == 'Paralelismo':
-                        exerciseSkillParalellism = skill['score']
+                        exercise_skill_paralellism = skill['score']
                     elif skill['name'] == 'Pensamiento lógico':
-                        exerciseSkillLogicalThinking = skill['score']
+                        exercise_skill_logical_thinking = skill['score']
                     elif skill['name'] == 'Control de flujo':
-                        exerciseSkillFlowControl = skill['score']
+                        exercise_skill_flow_control = skill['score']
                     elif skill['name'] == 'Interactividad con el usuario':
-                        exerciseSkillUserInteractivity = skill['score']
+                        exercise_skill_user_interactivity = skill['score']
                     elif skill['name'] == 'Representación de la información':
-                        exerciseSkillInformationRepresentation = skill['score']
+                        exercise_skill_information_representation = skill['score']
                     elif skill['name'] == 'Abstracción':
-                        exerciseSkillAbstraction = skill['score']
+                        exercise_skill_abstraction = skill['score']
                     elif skill['name'] == 'Sincronización':
-                        exerciseSkillSyncronization = skill['score']
-            if 'validSolution' in element['exercise']:
-                exerciseValidSolution = element['exercise']['validSolution']
+                        exercise_skill_syncronization = skill['score']
+            if 'valid_solution' in element['exercise']:
+                exercise_valid_solution = element['exercise']['valid_solution']
             if 'isEvaluation' in element['exercise']:
-                exerciseIsEvaluation = element['exercise']['isEvaluation']
+                exercise_is_evaluation = element['exercise']['isEvaluation']
             if 'level' in element['exercise']:
-                exerciseLevel = element['exercise']['level']
+                exercise_level = element['exercise']['level']
 
         #Solution distance information
         if 'solutionDistance' in element:
             if 'familyDistance' in element['solutionDistance']:
-                solutionDistanceFamilyDistance = element['solutionDistance']['familyDistance']
+                solution_distance_family_distance = element['solutionDistance']['familyDistance']
             if 'elementDistance' in element['solutionDistance']:
-                solutionDistanceElementDistance = element['solutionDistance']['elementDistance']
+                solution_distance_element_distance = element['solutionDistance']['elementDistance']
             if 'positionDistance' in element['solutionDistance']:
-                solutionDistancePositionDistance = element['solutionDistance']['positionDistance']
+                solution_distance_position_distance = element['solutionDistance']['positionDistance']
             if 'inputDistance' in element['solutionDistance']:
-                solutionDistanceInputDistance = element['solutionDistance']['inputDistance']
+                solution_distance_input_distance = element['solutionDistance']['inputDistance']
             if 'totalDistance' in element['solutionDistance']:
-                solutionDistanceTotalDistance = element['solutionDistance']['totalDistance']
+                solution_distance_total_distance = element['solutionDistance']['totalDistance']
 
-        if 'dateTime' in element:
-            dateTime = element['dateTime']
-        if 'requestHelp' in element:
-            requestHelp = element['requestHelp']
-        if 'secondsHelpOpen' in element:
-            secondsHelpOpen = element['secondsHelpOpen']
-        if 'finishedExercise' in element:
-            finishedExercise = element['finishedExercise']
-        if 'validSolution' in element:
-            validSolution = element['validSolution']
+        if 'request_help' in element:
+            request_help = element['request_help']
+        if 'seconds_help_open' in element:
+            seconds_help_open = element['seconds_help_open']
+        if 'finished_exercise' in element:
+            finished_exercise = element['finished_exercise']
+        if 'valid_solution' in element:
+            valid_solution = element['valid_solution']
         if 'grade' in element:
             grade = element['grade']
-        if 'lastLogin' in element:
-            lastLogin = element['lastLogin']
 
         # Creating  the row of the csv
-        rowList.append([studentGender, studentMotherTongue, studentAge, studentCompetence,
-                        studentMotivation, exerciseSkillParalellism, exerciseSkillLogicalThinking,
-                        exerciseSkillFlowControl, exerciseSkillUserInteractivity, exerciseSkillInformationRepresentation,
-                        exerciseSkillAbstraction, exerciseSkillSyncronization, exerciseValidSolution, exerciseIsEvaluation,
-                        exerciseLevel, solutionDistanceFamilyDistance, solutionDistanceElementDistance,
-                        solutionDistancePositionDistance, solutionDistanceInputDistance, solutionDistanceTotalDistance,
-                        dateTime, requestHelp, secondsHelpOpen, finishedExercise, validSolution, grade,
-                        lastLogin])
+        row_list.append([student_gender, student_mother_tongue, student_age, student_competence,
+                         student_motivation, exercise_skill_paralellism, exercise_skill_logical_thinking,
+                         exercise_skill_flow_control, exercise_skill_user_interactivity, exercise_skill_information_representation,
+                         exercise_skill_abstraction, exercise_skill_syncronization, exercise_valid_solution, exercise_is_evaluation,
+                         exercise_level, solution_distance_family_distance, solution_distance_element_distance,
+                         solution_distance_position_distance, solution_distance_input_distance, solution_distance_total_distance,
+                         request_help, seconds_help_open, finished_exercise, valid_solution, grade, total_seconds])
 
-    return rowList
+    return row_list
 
 
 # 1- Gets the json data
